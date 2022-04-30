d0f9cdffab61b5b228157b09215decc42d075d0e
Michał Penk <michal.penk@nexpertis.pl>
2022-04-30 21:41:02 +0200

    Add dark theme

--- src/App.js
+++ src/App.js
@@ -6,13 +6,17 @@ import { Statistics } from './pages/statistics';
 function App() {
   return (
     <BrowserRouter >
-        <Header />
-      <Routes >
-        <Route path='/' element={<Statistics />} exact />
-        <Route path='/404' element={<NotFound />} />
-        <Route path="*" element={<Navigate to="/404" />} />
-      </Routes >
-      <Footer/>
+     <div data-theme='dark'>
+      <Header />
+     
+        <Routes >
+          <Route path='/' element={<Statistics />} exact />
+          <Route path='/404' element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/404" />} />
+        </Routes >
+      <Footer />
+      </div>
+
     </BrowserRouter>
   );
 }
