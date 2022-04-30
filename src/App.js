46728a1bddc1288fec7d0dc37363959ae30323fd
Michał Penk <michal.penk@nexpertis.pl>
2022-04-30 22:10:47 +0200

    Change names

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { NotFound, Header, Footer } from './layouts';
+import {Admin } from  './pages/admin'
+import {Pool } from  './pages/pool'
 import { Statistics } from './pages/statistics';
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
      
         <Routes >
           <Route path='/' element={<Statistics />} exact />
+          <Route path='/admin' element={<Admin />} exact />
+          <Route path='/pool' element={<Pool/>} exact />
           <Route path='/404' element={<NotFound />} />
           <Route path="*" element={<Navigate to="/404" />} />
         </Routes >
--- src/pages/pool/index.js
+++ src/pages/pool/index.js
@@ -1,4 +1,4 @@
-export function Questionnaire(props) {
+export function Pool(props) {
 
     return (
         <>
