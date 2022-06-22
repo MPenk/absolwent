5d4d9fec99e6c49cd750dad358e3047c404b0543
kowalkos <kowalkewit@gmail.com>
2022-06-22 16:51:31 +0200

    fast commit

---
+++ src/components/statistics/charts/AgeGenderChart.js
@@ -0,0 +1,106 @@
+import axios from "axios";
+import ReactECharts from "echarts-for-react";
+import { useEffect, useState } from "react";
+
+function numberRange(start, end) {
+  return new Array(end - start).fill().map((d, i) => i + start);
+}
+
+export function AgeGenderChart(props) {
+  const [data, setData] = useState(props.dane);
+  const kategorie = props.kategoria;
+  const keysKategorie = [];
+  for (var key in kategorie) keysKategorie.push(key);
+  const [dane, setDane] = useState([]);
+
+  const [category, setCategory] = useState([]);
+  const [ans, setAns] = useState([]);
+  var endpoint =
+    "https://absolwent.azurewebsites.net/api/public/statistics/salary/";
+  const lata = numberRange(props.dane.lata[0], props.dane.lata[1] + 1);
+  useEffect(() => {
+    setAns([]);
+    setDane([]);
+    setCategory([]);
+    for (var x in lata) {
+      axios
+        .get(endpoint + lata[x],{crossdomain: true} )
+        .then((res) => {
+            console.log(res)
+          let answear = res.data.data.answers;
+          setAns(ans=>[...ans,answear]);
+          let cat = [];
+          let val = [];
+          for (var x in answear) {
+            cat.push(answear[x].name);
+            val.push(answear[x].count);
+          }
+          setDane(dane=>[...dane,val]);
+          setCategory(cat);
+        })
+    }
+  }, [props.dane.lata[0], props.dane.lata[1]]);
+  const [seriesData,setSeriesData]=useState([]);
+  useEffect(() => {
+    setSeriesData(dane.map((seria,i)=>{
+        return{
+            name: category[0],
+            data: seria,
+             emphasis: {
+        focus: 'series'
+      },
+            type:'bar',
+        }
+    }))
+
+  }, [dane]);
+  useEffect(() => {
+    console.log("ANSWER")
+   console.log(ans);
+
+  }, [ans]);
+  const option = {
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "shadow",
+      },
+    },
+    legend: {
+        data:category[0]
+    },
+    toolbox: {
+        show: true,
+        orient: 'vertical',
+        left: 'right',
+        top: 'center',
+        feature: {
+          mark: { show: true },
+          dataView: { show: true, readOnly: false },
+          magicType: { show: true, type: ['line', 'bar', 'stack'] },
+          restore: { show: true },
+          saveAsImage: { show: true }
+        }
+      },
+    grid: {
+      left: "3%",
+      right: "4%",
+      bottom: "3%",
+      containLabel: true,
+    },
+    xAxis: {
+      type: "category",
+      data: lata,
+    },
+    yAxis: {
+      type: "value",
+    },
+    series: seriesData
+  };
+
+  return (
+    <>
+      <ReactECharts option={option} className="bar-chart" />
+    </>
+  );
+}
--- src/pages/statistics/index.js
+++ src/pages/statistics/index.js
@@ -4,15 +4,67 @@ import { Box } from "@mui/system";
 import { Button,Checkbox } from "@mui/material";
 import { Professions } from "../../components/statistics/professions/Professions";
 import MenuBar from "../../components/statistics/MenuBar";
-
+import { GradFacultyToBusinessSize } from "../../components/statistics/charts/GradFacultyToBusinessSize";
+import { useEffect, useState } from "react";
+import { ChartCommon } from "../../components/statistics/charts/ChartCommon";
+import { AgeGenderChart } from "../../components/statistics/charts/AgeGenderChart";
 export function Statistics(props) {
 
+  const wielkoscFirmyTabela = [
+    "Mikroprzedsiębiorstwo (poniżej 10 pracowników)",
+    "Małe przedsiębiorstwo (poniżej 50 pracowników)",
+    "Średnie przedsiębiorstwo (poniżej 250 pracowników)",
+    "Duże przedsiębiorstwo (powyżej 250 pracowników)",
+  ];
+  const kategoriaFirmyTablica = [
+    "IT",
+    "Mechaniczna",
+    "Nawigacyjna",
+    "Ekonomiczna",
+    "Elektroniczna/Elektryczna",
+    "Gastronomia",
+    "Dietetyka",
+    "Marketing",
+    "Edukacja",
+    "inna",
+  ];
+  const wielkoscMiastaTabela = [
+    "Poniżej 10 000 mieszkańców",
+    "Od 10 000 do 50 000 mieszkańców",
+    "Od 50 000 do 100 000 mieszkańców",
+    "100 000 do 250 000 mieszkańców",
+    "Powyżej 250 000 mieszkańców",
+  ];
+  const wydzialTablica = ["Elektryczny", "Mechaniczny", "Nawigacyjny", "PiT"];
+  const plecTab = ["Kobieta", "Mężczyzna"];
+  const kategorie= {plec:plecTab,kategoria:kategoriaFirmyTablica,wydzial:wydzialTablica,miasto:wielkoscMiastaTabela,firma:wielkoscFirmyTabela};
+  const [data,setData]=useState('');
+  const childToParent=(childdata)=>{
+    setData(childdata)
+  }
+  let wykres;
+  useEffect(()=>{console.log(data)})
+  if(data==='')
+  {
+    wykres=<h1>Wybierz jakieś kryteria</h1>;
+  }
+  else if(data.plec[0] || data.plec[1])
+  {
+    wykres=<AgeGenderChart dane={data}  kategoria={kategorie}/>
+  }
+  else{
+    wykres=<AgeGenderChart dane={data}  kategoria={kategorie}/>;
+  }
   return (
     <>
       <Box sx={{ width: "100%", height: "auto", display: "flex" }}>
-       <MenuBar/>
-        <Box sx={{ width: "auto", float: "left", flexGrow: 1 }}>
+       <MenuBar childToParent={childToParent}/>
+        {/* <Box sx={{ width: "auto", float: "left", flexGrow: 1 }}>
           <Professions />
+        </Box> */}
+        
+        <Box sx={{ width: "auto", float: "left", flexGrow: 1 }}>
+          {wykres}
         </Box>
       </Box>
     </>
