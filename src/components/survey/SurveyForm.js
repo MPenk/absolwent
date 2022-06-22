83f0907844860be1d06a484c5f6099ab1ee7be71
eziaw <sewastianowjakub@gmail.com>
2022-06-22 19:21:26 +0200

    wysylanie ankiet

--- src/components/survey/SurveyForm.js
+++ src/components/survey/SurveyForm.js
@@ -6,18 +6,21 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormLabel from '@mui/material/FormLabel';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import InputLabel from '@mui/material/InputLabel';
 import { useSearchParams } from 'react-router-dom';
 import { Box } from '@mui/system';
 import { CustomTextField } from './CustomTextField';
 import { CustomSelect } from './CustomSelect';
 import { execute } from '../../api/connection';
+import Button from '@mui/material/Button';
+import { useNavigate } from "react-router-dom";
+import SendIcon from '@mui/icons-material/Send';
 
 export function SurveyForm() {
+    let navigate = useNavigate();
 
     var test = false;
     const [data, setData] = React.useState({
-        year: "",
+        graduationYear: "",
         field: "",
         faculty: "",
         title: "",
@@ -27,6 +30,7 @@ export function SurveyForm() {
 
     const [radioValue, setRadioValue] = React.useState('nie');
     const [proffesionalActivity, setProffesionalActivity] = React.useState(true);
+    const [selectValue, setSelectValue] = React.useState("");
     const handleChange = (event) => {
 
         data[event.target.name] = event.target.value;
@@ -35,20 +39,33 @@ export function SurveyForm() {
             setRadioValue(event.target.value);
             if (radioValue === "tak") {
                 setProffesionalActivity(true);
+                setSelectValue("on");
             }
             else {
                 setProffesionalActivity(false);
+                setSelectValue("");
             }
         }
     }
-
+    const handleSendButton = async () => {
+        console.log(data)
+        const response = await execute({ path: "/survey", requestMethod: "POST", data: {...data, token: searchParams.get("key")} });
+        if (response)
+            navigate("/survey/thanks");
+    }
     React.useEffect(() => {
         if (!test) {
             test = true;
             async function fetchData() {
+                console.log(searchParams.get("key"));
+
                 const response = await execute({ path: "/auth/survey", requestMethod: "POST", data: { token: searchParams.get("key") } });
-                if (response)
+                if (response){
+                    console.log(response.data);
                     setData(response.data);
+
+                }
+
             }
             fetchData();
 
@@ -62,7 +79,7 @@ export function SurveyForm() {
                     Ankieta
                 </Typography>
                 <Grid container spacing={3} alignItems="center" justifyContent="center">
-                    <CustomTextField label="Rok ukończenia studiów" value={data.year} />
+                    <CustomTextField label="Rok ukończenia studiów" value={data.graduationYear} />
                     <CustomTextField label="Kierunek studiów" value={data.field} />
                     <CustomTextField label="Wydział studiów" value={data.faculty} />
                     <CustomTextField label="Tytuł zawodowy" value={data.title} />
@@ -70,13 +87,13 @@ export function SurveyForm() {
                 </Grid>
             </FormControl>
 
-            <FormControl style={{width:"100%", my:3}} >
-                <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+            <FormControl style={{ width: "100%", my: 3 }} >
+                <div style={{ margin: '25px', display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
 
                     <Box >
                         <FormLabel id="akt_zawodowa">Czy jest Pani/Pan aktywny zawodowo?</FormLabel>
                         <RadioGroup
-                        id="123"
+                            id="123"
                             row
                             aria-labelledby="akt_zawodowa"
                             name="proffesionalActivity"
@@ -90,19 +107,19 @@ export function SurveyForm() {
                     </Box>
                 </div>
                 <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
-                    <CustomSelect dataName='earnings' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
+                    <CustomSelect value={selectValue} dataName='earnings' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
 
-                    <CustomSelect dataName="companySize" isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
-                    <CustomSelect dataName='townSize' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
+                    <CustomSelect value={selectValue} dataName="companySize" isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
+                    <CustomSelect value={selectValue} dataName='townSize' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
 
-                    <CustomSelect dataName='companyCategory' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
-                    <CustomSelect dataName='jobSearchTime' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
+                    <CustomSelect value={selectValue} dataName='companyCategory' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
+                    <CustomSelect value={selectValue} dataName='jobSearchTime' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
 
-                    <CustomSelect dataName='periodOfEmployment' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
+                    <CustomSelect value={selectValue} dataName='periodOfEmployment' isDisabled={proffesionalActivity} handleChangeParent={handleChange} minWidth={450} />
                 </div>
                 <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
 
-                    <Box sx={{ m: 3 , mx:5}}>
+                    <Box sx={{ m: 3, mx: 5 }}>
                         <FormLabel id="akt_zawodowa">Czy pracujesz na terenie Polski?</FormLabel>
                         <RadioGroup
                             row
@@ -114,7 +131,7 @@ export function SurveyForm() {
                             <FormControlLabel value="nie" control={<Radio />} label="Nie" disabled={proffesionalActivity} />
                         </RadioGroup>
                     </Box>
-                    <Box sx={{ m: 3, mx:5 }}>
+                    <Box sx={{ m: 3, mx: 5 }}>
 
                         <FormLabel id="akt_zawodowa">Czy jest Pani/Pan zadowolona/y ze swojej pracy?</FormLabel>
                         <RadioGroup
@@ -128,8 +145,15 @@ export function SurveyForm() {
                             <FormControlLabel value="trudno_powiedziec" control={<Radio />} label="Trudno stwierdzić" disabled={proffesionalActivity} />
                         </RadioGroup>
                     </Box>
-                </div>
 
+                </div>
+                <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+                    <Box sx={{ mb: 5, mx: 5 }}>
+                        <Button variant="contained" endIcon={<SendIcon />} onClick={handleSendButton}>
+                            Prześlij wypełnioną ankietę
+                        </Button>
+                    </Box>
+                </div>
             </FormControl>
         </div>
     );
